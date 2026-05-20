fbdac81ff143c0a20600b1a254ddb7d3b99dcecc
bernatrabassac <brabassac@edu.tecnocampus.cat>
2026-05-20 12:25:42 +0200

    correció errors

--- breakout_2526_v01_preprojecte/js/bola.js
+++ breakout_2526_v01_preprojecte/js/bola.js
@@ -5,6 +5,7 @@ class Bola {
         this.vx = 1;
         this.vy = -1;
         this.color = "#fff";
+      
     };
 
     draw(ctx) {
@@ -14,90 +15,82 @@ class Bola {
         ctx.fill();
         ctx.closePath();
     }
-
-    mou(x, y) {
+    mou(x,y){
         this.posicio.x += x;
         this.posicio.y += y;
     }
+    update(amplada, alcada){
 
-    // AFEGIM LA PALA COM A PARÀMETRE AQUÍ
-    update(amplada, alcada, pala) {
         let puntActual = this.posicio;
-        let puntSeguent = new Punt(this.posicio.x + this.vx, this.posicio.y + this.vy);
-        let trajectoria = new Segment(puntActual, puntSeguent);
+        let puntSeguent= new Punt(this.posicio.x + this.vx,
+                            this.posicio.y + this.vy);
+        let trajectoria= new Segment(puntActual, puntSeguent);
         let exces;
         let xoc = false;
         
-        // --- Xoc amb els laterals del canvas ---
-        
-        // Xoc lateral superior (eix Y)
-        if (trajectoria.puntB.y - this.radi < 0) {
-            exces = (trajectoria.puntB.y - this.radi) / this.vy;
-            this.posicio.x = trajectoria.puntB.x - exces * this.vx;
+
+        //Xoc amb els laterals del canvas
+        //Xoc lateral superior
+        if(trajectoria.puntB.y - this.radi < 0){
+            exces= (trajectoria.puntB.y - this.radi)/this.vy;
+            this.posicio.x = trajectoria.puntB.x - exces*this.vx;
             this.posicio.y = this.radi;
             xoc = true;
             this.vy = -this.vy;
         }
-        // Xoc lateral inferior (eix Y)
-        else if (trajectoria.puntB.y + this.radi > alcada) {
+        //Xoc lateral dret
+        else if(trajectoria.puntB.y + this.radi > alcada) {
             exces = (trajectoria.puntB.y + this.radi - alcada) / this.vy;
             this.posicio.x = trajectoria.puntB.x - exces * this.vx;
             this.posicio.y = alcada - this.radi;
             xoc = true;
             this.vy = -this.vy;
         }
-
-        // Xoc lateral esquerra (eix X)
-        if (trajectoria.puntB.x - this.radi < 0) {
+        //Xoc lateral esquerra
+        if(trajectoria.puntB.x - this.radi < 0){
             exces = (trajectoria.puntB.x - this.radi) / this.vx;
             this.posicio.x = this.radi;
-            this.posicio.y = trajectoria.puntB.y - exces * this.vy;
+            this.posicio.y = trajectoria.puntB.y - exces * this.vy; // aqui la Y es mou amb exces
             xoc = true;
             this.vx = -this.vx;
         }
-        // Xoc lateral dret (eix X)
-        else if (trajectoria.puntB.x + this.radi > amplada) {
+        //Xoc lateral inferior
+        else if(trajectoria.puntB.x + this.radi > amplada) {
             exces = (trajectoria.puntB.x + this.radi - amplada) / this.vx;
             this.posicio.x = amplada - this.radi;
             this.posicio.y = trajectoria.puntB.y - exces * this.vy;
             xoc = true;
             this.vx = -this.vx;
         }
+        //Xoc amb la pala
 
-        // --- Xoc amb la pala ---
-        let dadesColisio = this.interseccioSegmentRectangle(trajectoria, pala);
-
-        if (dadesColisio) {
-            xoc = true;
-            // Invertim la velocitat vertical perquè reboti
-            this.vy = -this.vy;
-            
-            // DUBTE PROFE: la bola al xocar amb la pala no faria coses rares?
-            this.posicio.x = trajectoria.puntB.x; 
-            this.posicio.y = trajectoria.puntB.y;
-        }
-
-        // --- Xoc amb els totxos del mur ---
-        // Utilitzem el mètode INTERSECCIOSEGMENTRECTANGLE
+        //Xoc amb els totxos del mur
+        
+        //Utilitzem el mètode INTERSECCIOSEGMENTRECTANGLE
         
 
-        // Actualització si no hi ha hagut cap xoc
-        if (!xoc) {
+        if (!xoc){
             this.posicio.x = trajectoria.puntB.x;
             this.posicio.y = trajectoria.puntB.y;
         }     
+        
     }
 
-    interseccioSegmentRectangle(segment, rectangle) {
+    interseccioSegmentRectangle(segment, rectangle){
+
        //1r REVISAR SI EXISTEIX UN PUNT D'INTERSECCIÓ EN UN DELS 4 SEGMENTS
+       //SI EXISTEIX, QUIN ÉS AQUEST PUNT
+       //si hi ha més d'un, el més ajustat
        let puntI;
        let distanciaI;
        let puntIMin;
        let distanciaIMin = Infinity;
        let voraI;
 
+       //calcular punt d'intersecció amb les 4 vores del rectangle
+       //necessitem coneixer els 4 segments del rectangle
        //vora superior
-       let segmentVoraSuperior = new Segment(rectangle.posicio,
+       let segmentVoraSuperior = new  Segment(rectangle.posicio,
            new Punt(rectangle.posicio.x + rectangle.amplada, rectangle.posicio.y));
        //vora inferior
       
@@ -107,6 +100,9 @@ class Bola {
       
 
        //2n REVISAR SI EXISTEIX UN PUNT D'INTERSECCIÓ EN UN DELS 4 SEGMENTS
+       //SI EXISTEIX, QUIN ÉS AQUEST PUNT
+       //si hi ha més d'n, el més ajustat
+    
        //vora superior
        puntI = segment.puntInterseccio(segmentVoraSuperior);
        if (puntI){
@@ -134,3 +130,4 @@ class Bola {
         return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
     }
 }
+
